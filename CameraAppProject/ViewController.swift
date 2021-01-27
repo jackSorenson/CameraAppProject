34dbb61093092216b119fc5d3566f4519afc0807
Jack Sorenson <jack.sorenson@gmail.com>
2021-01-27 10:34:00 -0500

    Initial Commit

---
+++ CameraAppProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraAppProject
+//
+//  Created by Jack Sorenson on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
